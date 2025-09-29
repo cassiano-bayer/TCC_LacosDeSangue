a6180c9e359e3d0fb6830d64f4fff5f8db2a44e5
cassiano-bayer <cassiano.bayer@gmail.com>
2025-09-29 20:42:39 -0300

    sistema.php

--- sistema.php
+++ sistema.php
@@ -36,7 +36,7 @@
     $result = $conexao->query($sql);
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -669,4 +669,5 @@
     
     </script>
 </body>
+
 </html>
